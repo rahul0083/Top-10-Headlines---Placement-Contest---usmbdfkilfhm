1c30e23ff1e5364a171d9b6d813510f133c33388
Rahul Singh <rahulme0083@gmail.com>
2023-04-02 16:08:34 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,29 @@ import '../styles/App.css';
 const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
+   
+  const handleCategory=(e)=>{
+    setCategory(e.target.value);
+  }
+
+  useEffect(()=>{
+   const fetchData=async()=>{
+     setLoading(true);
+     const response=await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=6623e4a890e07e81cca647e7bd663aae&max=10&lan=en`);
+     const data=await response.json();
+     setNewsData(data.articles);
+     setLoading(false);
+   };
+      fetchData();
+  },[category])
+
 
   return (
     <div id="main">
       <h1 className='heading'>Top 10 {category} news.</h1>
-      <select value={category}>
-        <option value="general">General</option>
+      <select value={category} onChange={handleCategory}>
+        <option value="general" >General</option>
         <option value="business">Business</option>
         <option value="sports">Sports</option>
         <option value="technology">Technology</option>
@@ -18,19 +34,23 @@ const App = () => {
         <option value="entertainment">Entertainment</option>
         <option value="science">Science</option>
       </select>
-      <p className='loader'>Loading...</p>
+      {loading ?(
+      <p className='loader'>Loading...</p>):(
       <ol>
-        <li key="">
-          <img className='news-img' src="" alt=""/>
+        {newsData.map((articles)=>(
+        <li key={articles.title}>
+          <img className='news-img' src={articles.image}alt={articles.title}/>
           <section className='new-title-content-author'>
-            <h3 className='news-title'>news title</h3>
+            <h3 className='news-title'>{articles.title}</h3>
             <section className='new-content-author'>
-              <p className='news-description'>news description</p>
-              <p className='news-source'><strong>Source:</strong> source name</p>
+              <p className='news-description'>{articles.description}</p>
+              <p className='news-source'><strong>Source:</strong>{articles.source.name}</p>
             </section>
           </section>
         </li>
+        ))}
       </ol>
+      )}
     </div>
   )
 }
